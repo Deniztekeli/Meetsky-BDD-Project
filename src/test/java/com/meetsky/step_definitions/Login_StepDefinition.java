889a1f3ecbe645c2f57a6d90c4e1d75f142379ee
Deniz <denizztekeli@gmail.com>
2022-11-10 14:39:19 +0100

    New updates

--- src/test/java/com/meetsky/step_definitions/Login_StepDefinition.java
+++ src/test/java/com/meetsky/step_definitions/Login_StepDefinition.java
@@ -1,12 +1,11 @@
 package com.meetsky.step_definitions;
 
-import com.meetsky.pages.BasePage;
-import io.cucumber.java.en.And;
+import com.meetsky.pages.LoginPage;
 import io.cucumber.java.en.Given;
 
 public class Login_StepDefinition {
 
-    BasePage basePage = new BasePage();
+    LoginPage basePage = new LoginPage();
 
     @Given("the user is on the login page")
     public void the_user_is_on_the_login_page() {
