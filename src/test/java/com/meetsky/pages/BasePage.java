336da6cb0e5a1c6bfa46be513bfb1f2f20639ef7
Deniz <denizztekeli@gmail.com>
2022-11-10 09:20:03 +0100

    Some changes are done

--- src/test/java/com/meetsky/pages/BasePage.java
+++ src/test/java/com/meetsky/pages/BasePage.java
@@ -20,10 +20,16 @@ public class BasePage {
     @FindBy(id = "submit-form")
     public WebElement loginButton;
 
+    /*
+
+
     public void login(){
+
         Driver.getDriver().get(ConfigurationReader.getProperty("meetsky.url"));
-        usernameButton.sendKeys(ConfigurationReader.getProperty("meetsky.username"));
+        usernameButton.sendKeys(ConfigurationReader.getProperty("meetsky.username.deniz"));
         passwordButton.sendKeys(ConfigurationReader.getProperty("meetsky.password"));
         loginButton.click();
     }
-}
+    */
+    }
+
--- target/test-classes/features/login.feature
+++ target/test-classes/features/login.feature
@@ -1,6 +1,5 @@
-Feature: Files features
-  As a user, I should be able to add a file to favorites, rename any file and give some comments on any file.
+Feature: Login features
 
-  Background:
+Scenario: 
     Given the user is on the login page
-    And the user enters the files menu
+
