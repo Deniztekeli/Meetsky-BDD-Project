a26cf561b84b963189fa9818c0b6832d39882558
elnurakhundov <elnurakhundov55@gmail.com>
2022-11-17 19:37:56 +0100

    Finished

--- src/test/java/com/meetsky/step_definitions/Login_StepDefinition.java
+++ src/test/java/com/meetsky/step_definitions/Login_StepDefinition.java
@@ -2,7 +2,6 @@ package com.meetsky.step_definitions;
 
 import com.meetsky.pages.LoginPage;
 import com.meetsky.pages.ProfileSettingsPage;
-import com.meetsky.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 
@@ -11,24 +10,15 @@ public class Login_StepDefinition {
     LoginPage basePage = new LoginPage();
     ProfileSettingsPage profileSettingsPage = new ProfileSettingsPage();
 
-    @Given("the user is on the login page")
-    public void the_user_is_on_the_login_page() {
 
-        Driver.getDriver().get("https://qa.meetsky.net/index.php/login?clear=1");
-        basePage.usernameButton.sendKeys("Employee91");
-        basePage.passwordButton.sendKeys("Employee123");
-        basePage.loginButton.click();
 
-        /*
+
         LoginPage loginPage = new LoginPage();
     @Given("the user is on the login page")
     public void the_user_is_on_the_login_page() {
         loginPage.login();
     }
 
-         */
-
-    }
     @When("the user clicks on the Profile button")
     public void the_user_clicks_on_the_profile_button() {
         profileSettingsPage.profileButton.click();
