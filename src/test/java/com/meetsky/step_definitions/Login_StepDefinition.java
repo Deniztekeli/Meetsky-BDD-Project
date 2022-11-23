b819e026b9f8ef33731fed84bbf1acbc0d5f8f97
elnurakhundov <elnurakhundov55@gmail.com>
2022-11-23 22:45:18 +0100

    Finished

--- src/test/java/com/meetsky/step_definitions/Login_StepDefinition.java
+++ src/test/java/com/meetsky/step_definitions/Login_StepDefinition.java
@@ -9,10 +9,6 @@ public class Login_StepDefinition {
 
     LoginPage loginPage = new LoginPage();
 
-
-
-
-        LoginPage loginPage = new LoginPage();
     @Given("the user is on the login page")
     public void the_user_is_on_the_login_page() {
         loginPage.login();
