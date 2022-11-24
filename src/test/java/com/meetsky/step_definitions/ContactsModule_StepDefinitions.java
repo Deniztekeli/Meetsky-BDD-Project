d0c3b2e2f4f0351ae6aad206fe6f70f066bfe42a
Rudi <rudidunga@gmail.com>
2022-11-24 15:33:02 +0100

    contacts module

---
+++ src/test/java/com/meetsky/step_definitions/ContactsModule_StepDefinitions.java
@@ -0,0 +1,95 @@
+package com.meetsky.step_definitions;
+
+import com.meetsky.pages.BasePage;
+import com.meetsky.pages.ContactGroupPage;
+import com.meetsky.pages.ContactsModulePage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ContactsModule_StepDefinitions {
+
+    BasePage basePage = new BasePage();
+    ContactsModulePage contactsModulePage = new ContactsModulePage();
+
+    @Given("the user is on the contacts module page")
+    public void the_user_is_on_the_contacts_module_page() {
+
+    }
+
+    @Given("the user enters the contacts menu")
+    public void the_user_enters_the_contacts_menu() {
+
+    }
+
+    @When("user click on new contact")
+    public void user_click_on_new_contact() {
+
+    }
+
+    @When("user put first name and last name of the contact here")
+    public void user_put_first_name_and_last_name_of_the_contact_here() {
+
+    }
+
+    @Then("user should able to create a new contact appears in the All contacts list with matching initials")
+    public void user_should_able_to_create_a_new_contact_appears_in_the_all_contacts_list_with_matching_initials() {
+
+    }
+
+    @When("user click on All contacts")
+    public void user_click_on_all_contacts() {
+
+    }
+
+    @When("user should able to see all contacts")
+    public void user_should_able_to_see_all_contacts() {
+
+    }
+
+    @Then("user should see the total number as well")
+    public void user_should_see_the_total_number_as_well() {
+
+    }
+
+    @When("user click on the Any name on the contact menu")
+    public void user_click_on_the_any_name_on_the_contact_menu() {
+
+    }
+
+    @When("select image icon")
+    public void select_image_icon() {
+
+    }
+
+    @When("click on the option Choose from files")
+    public void click_on_the_option_choose_from_files() {
+
+    }
+
+    @When("choose any photo from archive")
+    public void choose_any_photo_from_archive() {
+
+    }
+
+    @Then("user click on Choose button")
+    public void user_click_on_choose_button() {
+
+    }
+
+    @When("user click on the any contact name under Contacts icon")
+    public void user_click_on_the_any_contact_name_under_contacts_icon() {
+
+    }
+
+    @When("click on three vertical ellipsis on the right side of the page")
+    public void click_on_three_vertical_ellipsis_on_the_right_side_of_the_page() {
+
+    }
+
+    @Then("click Delete button")
+    public void click_delete_button() {
+
+    }
+
+}
